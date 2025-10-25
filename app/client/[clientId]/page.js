1aacf8c65a637a68fd2f35b7cedcb8b00dc14818
ViktorChizh <victor.chizh@mail.ru>
2025-10-25 10:29:29 +0300

    fix use params

--- app/client/[clientId]/page.js
+++ app/client/[clientId]/page.js
@@ -1,8 +1,8 @@
 import { use } from 'react'
 
 export default function ClientId({ params }) {
-  const resolvedParams = use(params)
+  const client = use(params)
   return (
-    <h1>СТРАНИЦА КЛИЕНТА {resolvedParams.clientId}</h1>
+    <h1>СТРАНИЦА КЛИЕНТА {client.clientId}</h1>
   )
 }
