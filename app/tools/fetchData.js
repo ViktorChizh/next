d8debdbc01331f30fdd23f5ddfef201c1492ea18
ViktorChizh <victor.chizh@mail.ru>
2025-10-26 17:04:58 +0300

    feat: get data

--- app/tools/fetchData.js
+++ app/tools/fetchData.js
@@ -1,5 +1,4 @@
 export default async function fetchData() {
   const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-  const result = await res.json()
-  return result
+  return await res.json()
  }
