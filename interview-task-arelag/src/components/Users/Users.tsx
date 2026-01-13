f420e71d8cc4153e1f44b4b09ce7b6c60d0fefd7
ViktorChizh <victor.chizh@mail.ru>
2026-01-13 12:52:45 +0300

    translation of styles to tailwind

--- interview-task-arelag/src/components/Users/Users.tsx
+++ interview-task-arelag/src/components/Users/Users.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState, type ChangeEvent } from "react";
 import { searchUsers, getEmailDomains, type User } from "../../backend";
 import { UserCard } from "../userCard/UserCard";
-import "./users.css";
 
 export const Users = () => {
   const [usersArr, setUsersArr] = useState<User[]>([]);
@@ -18,22 +17,23 @@ export const Users = () => {
     setSearchText(e.currentTarget.value);
   const domains = getEmailDomains();
   return (
-    <div className="main">
+    <div className="flex flex-col w-[50vw] p-2.5">
       <p>Поиск</p>
-
       <input
-        className="inputSearch"
+        className="w-full border-2 border-gray-500 rounded-md p-1.5 text-base  mb-5"
         placeholder="Введите имя, email или компанию..."
         type="text"
         value={searchText}
         onChange={onChangeHandler}
         autoFocus
       />
-
-      <div className="filters">
-        <div className="filtersDomain">
+      <div className="w-full flex justify-between mb-5 mt-5">
+        <div className="w-1/4 flex flex-col gap-1.5">
           <p>Email домен</p>
-          <select className="domain" title="Email домен">
+          <select
+            className="border border-gray-500 rounded-md text-base  p-1.5"
+            title="Email домен"
+          >
             {domains.map((d) => (
               <option key={d} value={d}>
                 {d}
@@ -41,28 +41,28 @@ export const Users = () => {
             ))}
           </select>
         </div>
-        <div className="filtersStatus">
+        <div className="w-1/2 flex flex-col gap-2.5 [&>div>label:not(:first-child)]:ml-5 [&>div>label>span]:ml-1.5">
           <p>Статус</p>
           <div>
             <label>
               <input type="radio" name="status" value="all" />
-              Все
+              <span>Все</span>
             </label>
 
             <label>
               <input type="radio" name="status" value="active" />
-              Активные
+              <span>Активные</span>
             </label>
 
             <label>
               <input type="radio" name="status" value="inactive" />
-              Неактивные
+              <span>Неактивные</span>
             </label>
           </div>
         </div>
       </div>
 
-      <div className="usersBlock">
+      <div className="w-full flex flex-wrap justify-between">
         {error ? (
           <p>Получена ошибка: {error}</p>
         ) : usersArr.length ? (
--- interview-task-arelag/src/components/userCard/UserCard.tsx
+++ interview-task-arelag/src/components/userCard/UserCard.tsx
@@ -1,29 +1,34 @@
 import { type User } from "../../backend";
-import "./usersCard.css";
 
 type Props = {
   user: User;
 };
 export const UserCard = ({ user }: Props) => {
   return (
-    <div className="card">
-      <div className="userInfo">
-        <div className="nameBlock">
-          <img src={user.avatar} alt="user avatar" className="ava" />
+    <div className="flex flex-col w-1/3 min-w-68.75 p-2.5 m-1.5 border border-gray-500 rounded-xl">
+      <div className="flex flex-nowrap w-full pb-2.5 border-b border-gray-500 mb-1.5">
+        <div className="flex flex-nowrap gap-2.5 w-full [&>div>p:first-child]:font-bold [&>div>p:first-child]:mb-1.5">
+          <img
+            src={user.avatar}
+            alt="user avatar"
+            className="w-12.5 h-12.5 rounded-full"
+          />
           <div>
             <p>{user.name}</p>
             <p>{user.role}</p>
           </div>
         </div>
         <div
-          className={`userStatus ${user.status === "active" ? "active" : ""}`}
+          className={`w-2.5 h-2.5 rounded-full border border-gray-500 justify-self-end ${
+            user.status === "active" ? "border-0 bg-green-600" : ""
+          }`}
         />
       </div>
-      <div className="infoBlock">
+      <div className="flex w-full justify-between mt-1.5">
         <span>Email:</span>
         <b>{user.email}</b>
       </div>
-      <div className="infoBlock">
+      <div className="flex w-full justify-between mt-1.5">
         <span>Компания:</span>
         <b>{user.company}</b>
       </div>
